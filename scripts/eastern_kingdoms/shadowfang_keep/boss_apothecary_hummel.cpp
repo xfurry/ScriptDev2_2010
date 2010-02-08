dccb6753a59c2b4a281d888d5a9ce2be5c286000
Xfurry <radu_cantor@yahoo.com>
2010-02-09 00:19:49 +0200

    Update for Apothecary Hummel

--- scripts/eastern_kingdoms/shadowfang_keep/boss_apothecary_hummel.cpp
+++ scripts/eastern_kingdoms/shadowfang_keep/boss_apothecary_hummel.cpp
@@ -48,41 +48,19 @@ enum
 	QUEST_BEEN_SERVED			= 14488,
 };
 
-struct MANGOS_DLL_DECL boss_apothecary_hummelAI : public ScriptedAI
+//Positional defines 
+struct LocationsXY
 {
-    boss_apothecary_hummelAI(Creature* pCreature) : ScriptedAI(pCreature) 
-	{
-		Reset();
-	}
-
-	uint32 m_uiBaxterTimer;
-	uint32 m_uiFryeTimer;
-
-    void Reset()
-    {
-		m_uiBaxterTimer		= 10000;
-		m_uiFryeTimer		= 20000;
-		DoCast(m_creature, SPELL_ALLURING_PERFUME);
-		m_creature->setFaction(35);
-    }
-
-    void JustDied(Unit* pKiller)
-    {
-    }
-
-    void UpdateAI(const uint32 uiDiff)
-    {
-        if (!m_creature->SelectHostileTarget() || !m_creature->getVictim())
-            return;
-
-        DoMeleeAttackIfReady();
-    }
+    float x, y, z;
+    uint32 id;
 };
 
-CreatureAI* GetAI_boss_apothecary_hummel(Creature* pCreature)
+static LocationsXY PositionLoc[]=
 {
-    return new boss_apothecary_hummelAI(pCreature);
-}
+    {-238.335f, 2164.789f, 89.714f},
+    {-167.565f, 2219.466f, 80.964f},
+    {-214.742f, 2239.666f, 79.765f},
+};
 
 struct MANGOS_DLL_DECL boss_apothecary_baxterAI : public ScriptedAI
 {
@@ -91,20 +69,87 @@ struct MANGOS_DLL_DECL boss_apothecary_baxterAI : public ScriptedAI
 		Reset();
 	}
 
+	uint32 m_uiSprayTimer;
+	uint32 m_uiReactionTimer;
+	uint32 m_uiSummonTimer;
+	bool m_bSummon;
+
     void Reset()
     {
+		m_uiSprayTimer		= 7000;
+		m_uiReactionTimer	= 15000;
+		m_uiSummonTimer		= 5000;
+		m_bSummon			= false;
 		DoCast(m_creature, SPELL_IRRESISTIBLE_COLOGNE);
 		m_creature->setFaction(35);
     }
 
+	void JustReachedHome()
+	{
+		if(Creature* pFrye = GetClosestCreatureWithEntry(m_creature, NPC_FRYE, 50.0f))
+		{
+			if(!pFrye->isAlive())
+				pFrye->Respawn();
+		}
+		if(Creature* pHummel = GetClosestCreatureWithEntry(m_creature, NPC_HUMMEL, 50.0f))
+		{
+			if(!pHummel->isAlive())
+				pHummel->Respawn();
+		}
+	}
+
     void JustDied(Unit* pKiller)
     {
+		bool bAddsDead = true;
+		if(Creature* pFrye = GetClosestCreatureWithEntry(m_creature, NPC_FRYE, 50.0f))
+		{
+			if(pFrye->isAlive())
+				bAddsDead = false;
+		}
+
+		if(!bAddsDead)
+			return;
+
+		if(Creature* pHummel = GetClosestCreatureWithEntry(m_creature, NPC_HUMMEL, 50.0f))
+		{
+			if(!pHummel->isAlive())
+				pHummel->SetFlag(UNIT_DYNAMIC_FLAGS, UNIT_DYNFLAG_LOOTABLE);
+		}
     }
+
+	void JustSummoned(Creature* pSummon)
+	{
+		pSummon->setFaction(14);
+		pSummon->AI()->AttackStart(m_creature->getVictim());
+	}
 
     void UpdateAI(const uint32 uiDiff)
     {
         if (!m_creature->SelectHostileTarget() || !m_creature->getVictim())
             return;
+
+		if(m_uiSprayTimer < uiDiff)
+		{
+			DoCast(m_creature->getVictim(), SPELL_COLOGNE_SPRAY);
+			m_uiSprayTimer = urand(5000, 7000);
+		}
+		else m_uiSprayTimer -= uiDiff;
+
+		if(m_uiReactionTimer < uiDiff)
+		{
+			m_creature->InterruptNonMeleeSpells(true);
+			DoCast(m_creature, SPELL_CHAIN_REACTION);
+			m_uiReactionTimer = urand(10000, 13000);
+		}
+		else m_uiReactionTimer -= uiDiff;
+
+		if(m_uiSummonTimer < uiDiff && m_bSummon)
+		{
+			for(uint8 i = 0; i < 3; i++)
+				m_creature->SummonCreature(NCP_CRAZED_APOTHECARY, PositionLoc[i].x, PositionLoc[i].y, PositionLoc[i].z, 0, TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 30000);
+			m_uiSummonTimer = 10000;
+		}
+		else m_uiSummonTimer -= uiDiff;
 
         DoMeleeAttackIfReady();
     }
@@ -122,19 +167,104 @@ struct MANGOS_DLL_DECL boss_apothecary_fryeAI : public ScriptedAI
 		Reset();
 	}
 
+	uint32 m_uiSpillTimer;
+	uint32 m_uiSummonTimer;
+	bool m_bSummon;
+
     void Reset()
     {
 		m_creature->setFaction(35);
+		m_uiSpillTimer = 5000;
+		m_uiSummonTimer	= 5000;
+		m_bSummon		= false;
     }
+
+	void JustReachedHome()
+	{
+		if(Creature* pBaxter = GetClosestCreatureWithEntry(m_creature, NPC_BAXTER, 50.0f))
+		{
+			if(!pBaxter->isAlive())
+				pBaxter->Respawn();
+		}
+		if(Creature* pHummel = GetClosestCreatureWithEntry(m_creature, NPC_HUMMEL, 50.0f))
+		{
+			if(!pHummel->isAlive())
+				pHummel->Respawn();
+		}
+	}
+
+	void AttackStart(Unit* pWho)
+    {
+        if (!pWho) 
+            return;
+
+        if (m_creature->Attack(pWho, true))
+        {
+            m_creature->AddThreat(pWho);
+            m_creature->SetInCombatWith(pWho);
+            pWho->SetInCombatWith(m_creature);
+            DoStartMovement(pWho, 20.0f);
+        }
+    }
+
+	void JustSummoned(Creature* pSummon)
+	{
+		pSummon->setFaction(14);
+		pSummon->AI()->AttackStart(m_creature->getVictim());
+	}
 
     void JustDied(Unit* pKiller)
     {
+		bool bAddsDead = true;
+		if(Creature* pBaxter = GetClosestCreatureWithEntry(m_creature, NPC_BAXTER, 50.0f))
+		{
+			if(pBaxter->isAlive())
+			{
+				((boss_apothecary_baxterAI*)pBaxter->AI())->m_bSummon = true;
+				bAddsDead = false;
+			}
+		}
+
+		if(!bAddsDead)
+			return;
+
+		if(Creature* pHummel = GetClosestCreatureWithEntry(m_creature, NPC_HUMMEL, 50.0f))
+		{
+			if(!pHummel->isAlive())
+				pHummel->SetFlag(UNIT_DYNAMIC_FLAGS, UNIT_DYNFLAG_LOOTABLE);
+		}
     }
 
     void UpdateAI(const uint32 uiDiff)
     {
         if (!m_creature->SelectHostileTarget() || !m_creature->getVictim())
             return;
+
+		if(m_uiSpillTimer < uiDiff)
+		{
+			if (Unit* pTarget = m_creature->SelectAttackingTarget(ATTACKING_TARGET_RANDOM, 0))
+			{
+				switch(urand(0, 1))
+				{
+				case 0:
+					DoCast(pTarget, SPELL_PERFUME_SPILL);
+					break;
+				case 1:
+					DoCast(pTarget, SPELL_COLOGNE_SPILL);
+					break;
+				}
+			}
+			m_uiSpillTimer = 10000;
+		}
+		else m_uiSpillTimer -= uiDiff;
+
+		if(m_uiSummonTimer < uiDiff && m_bSummon)
+		{
+			for(uint8 i = 0; i < 3; i++)
+				m_creature->SummonCreature(NCP_CRAZED_APOTHECARY, PositionLoc[i].x, PositionLoc[i].y, PositionLoc[i].z, 0, TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 30000);
+			m_uiSummonTimer = 10000;
+		}
+		else m_uiSummonTimer -= uiDiff;
 
         DoMeleeAttackIfReady();
     }
@@ -143,6 +273,137 @@ struct MANGOS_DLL_DECL boss_apothecary_fryeAI : public ScriptedAI
 CreatureAI* GetAI_boss_apothecary_frye(Creature* pCreature)
 {
     return new boss_apothecary_fryeAI(pCreature);
+}
+
+struct MANGOS_DLL_DECL boss_apothecary_hummelAI : public ScriptedAI
+{
+    boss_apothecary_hummelAI(Creature* pCreature) : ScriptedAI(pCreature) 
+	{
+		Reset();
+	}
+
+	uint32 m_uiBaxterTimer;
+	uint32 m_uiFryeTimer;
+	bool m_bBaxterAggro;
+	bool m_bFryeAggro;
+
+	uint32 m_uiSprayTimer;
+	uint32 m_uiReactionTimer;
+
+	uint32 m_uiSummonTimer;
+
+    void Reset()
+    {
+		m_uiBaxterTimer		= 10000;
+		m_uiFryeTimer		= 20000;
+		m_bBaxterAggro		= false;
+		m_bFryeAggro		= false;
+
+		m_uiSprayTimer		= 7000;
+		m_uiReactionTimer	= 15000;
+
+		m_uiSummonTimer		= 5000;
+		DoCast(m_creature, SPELL_ALLURING_PERFUME);
+		m_creature->setFaction(35);
+    }
+
+	void JustReachedHome()
+	{
+		if(Creature* pBaxter = GetClosestCreatureWithEntry(m_creature, NPC_BAXTER, 50.0f))
+		{
+			if(!pBaxter->isAlive())
+				pBaxter->Respawn();
+		}
+		if(Creature* pFrye = GetClosestCreatureWithEntry(m_creature, NPC_FRYE, 50.0f))
+		{
+			if(!pFrye->isAlive())
+				pFrye->Respawn();
+		}
+	}
+
+    void JustDied(Unit* pKiller)
+    {
+		bool bAddsDead = true;
+		if(Creature* pBaxter = GetClosestCreatureWithEntry(m_creature, NPC_BAXTER, 50.0f))
+		{
+			if(pBaxter->isAlive())
+				bAddsDead = false;
+		}
+		if(Creature* pFrye = GetClosestCreatureWithEntry(m_creature, NPC_FRYE, 50.0f))
+		{
+			if(pFrye->isAlive())
+			{
+				((boss_apothecary_fryeAI*)pFrye->AI())->m_bSummon = true;
+				bAddsDead = false;
+			}
+		}
+		if(!bAddsDead)
+			m_creature->RemoveFlag(UNIT_DYNAMIC_FLAGS, UNIT_DYNFLAG_LOOTABLE);
+    }
+
+	void JustSummoned(Creature* pSummon)
+	{
+		pSummon->setFaction(14);
+		pSummon->AI()->AttackStart(m_creature->getVictim());
+	}
+
+    void UpdateAI(const uint32 uiDiff)
+    {
+        if (!m_creature->SelectHostileTarget() || !m_creature->getVictim())
+            return;
+
+		if(m_uiBaxterTimer < uiDiff && !m_bBaxterAggro)
+		{
+			if(Creature* pTemp = GetClosestCreatureWithEntry(m_creature, NPC_BAXTER, 50.0f))
+			{
+				pTemp->setFaction(14);
+				pTemp->AI()->AttackStart(m_creature->getVictim());
+			}
+			m_bBaxterAggro = true;
+		}
+		else m_uiBaxterTimer -= uiDiff;
+
+		if(m_uiFryeTimer < uiDiff && !m_bFryeAggro)
+		{
+			if(Creature* pTemp = GetClosestCreatureWithEntry(m_creature, NPC_FRYE, 50.0f))
+			{
+				pTemp->setFaction(14);
+				pTemp->AI()->AttackStart(m_creature->getVictim());
+			}
+			m_bFryeAggro = true;
+		}
+		else m_uiFryeTimer -= uiDiff;
+
+		if(m_uiSprayTimer < uiDiff)
+		{
+			DoCast(m_creature->getVictim(), SPELL_PERFUME_SPRAY);
+			m_uiSprayTimer = urand(5000, 7000);
+		}
+		else m_uiSprayTimer -= uiDiff;
+
+		if(m_uiReactionTimer < uiDiff)
+		{
+			m_creature->InterruptNonMeleeSpells(true);
+			DoCast(m_creature, SPELL_CHAIN_REACTION);
+			m_uiReactionTimer = urand(10000, 13000);
+		}
+		else m_uiReactionTimer -= uiDiff;
+
+		if(m_uiSummonTimer < uiDiff)
+		{
+			for(uint8 i = 0; i < 3; i++)
+				m_creature->SummonCreature(NCP_CRAZED_APOTHECARY, PositionLoc[i].x, PositionLoc[i].y, PositionLoc[i].z, 0, TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 30000);
+			m_uiSummonTimer = 10000;
+		}
+		else m_uiSummonTimer -= uiDiff;
+
+        DoMeleeAttackIfReady();
+    }
+};
+
+CreatureAI* GetAI_boss_apothecary_hummel(Creature* pCreature)
+{
+    return new boss_apothecary_hummelAI(pCreature);
 }
 
 struct MANGOS_DLL_DECL mob_crazed_apothecaryAI : public ScriptedAI
@@ -171,6 +432,18 @@ CreatureAI* GetAI_mob_crazed_apothecary(Creature* pCreature)
     return new mob_crazed_apothecaryAI(pCreature);
 }
 
+bool ChooseReward_apothecary_hummel(Player* pPlayer, Creature* pCreature, const Quest* pQuest, uint32 item)
+{
+	if(pQuest->GetQuestId() == QUEST_BEEN_SERVED)
+	{
+		pCreature->setFaction(14);
+		pCreature->AddThreat(pPlayer, 1000.0f);
+		pCreature->AI()->AttackStart(pPlayer);
+	}
+
+	return true;
+}
+
 void AddSC_boss_apothecarey_hummel()
 {
     Script* NewScript;
@@ -178,6 +451,7 @@ void AddSC_boss_apothecarey_hummel()
     NewScript = new Script;
     NewScript->Name = "boss_apothecary_hummel";
     NewScript->GetAI = GetAI_boss_apothecary_hummel;
+	NewScript->pChooseReward = &ChooseReward_apothecary_hummel;
     NewScript->RegisterSelf();
 
 	NewScript = new Script;
