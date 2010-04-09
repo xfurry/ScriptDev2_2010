02309103ae4a68253845745c3622dd7bc8ca8344
Xfurry <mail@nomail.com>
2010-04-10 00:26:12 +0300

    [SP_08] Pit of Saron

---
+++ sql/Custom_Updates/ScriptDev2/Northrend/PitOfSaron_Script.sql
@@ -0,0 +1,74 @@
+/* PIT OF SARON */
+-- Intro
+-- Jaina
+INSERT INTO `script_texts` VALUES ('-1610042', 'Heroes of the Alliance, attack!', '', '', '', '', '', '', '', '', 16626, '1', '0', '0', 'jaina_intro1');
+INSERT INTO `script_texts` VALUES ('-1610043', 'No! You monster!', '', '', '', '', '', '', '', '', 16627, '1', '0', '0', 'jaina_intro2');
+INSERT INTO `script_texts` VALUES ('-1610044', 'I do what I must. Please forgive me noble soldiers!', '', '', '', '', '', '', '', '', 16628, '1', '0', '0', 'jaina_intro3');
+INSERT INTO `script_texts` VALUES ('-1610045', 'You will have to make your way across this quary on your own!', '', '', '', '', '', '', '', '', 16629, '1', '0', '0', 'jaina_intro4');
+INSERT INTO `script_texts` VALUES ('-1610046', 'Free any alliance slaves that you come across! We will certianly need their assistance in battling Tyrannus. I will gather reinforcements and join you on the other side of the quary.', '', '', '', '', '', '', '', '', 16630, '1', '0', '0', 'jaina_intro5');
+-- Sylvanas
+INSERT INTO `script_texts` VALUES ('-1610047', 'Soldiers of the Horde, attack!', '', '', '', '', '', '', '', '', 17045, '1', '0', '0', 'sylvanas_intro1');
+INSERT INTO `script_texts` VALUES ('-1610048', 'Pathetic weaklings!', '', '', '', '', '', '', '', '', 17046, '1', '0', '0', 'sylvanas_intro2');
+INSERT INTO `script_texts` VALUES ('-1610049', 'You will have to battle you way through this seespate on your own.', '', '', '', '', '', '', '', '', 17047, '1', '0', '0', 'sylvanas_intro3');
+INSERT INTO `script_texts` VALUES ('-1610050', 'Free any horde slaves that you come across. We will certianly need their assistance in battling Tyrannus. I will gather reinforcements and join you on the other side of the quary.', '', '', '', '', '', '', '', '', 17048, '1', '0', '0', 'sylvanas_intro4');
+-- Tyrannus intro:
+INSERT INTO `script_texts` VALUES ('-1610200', 'Intruders have entered the master\'s domain. Signal the alarms!', '', '', '', '', '', '', '', '', 16747, '1', '0', '0', 'tyrannus_intro1');
+INSERT INTO `script_texts` VALUES ('-1610201', 'Not even fit to labor in a quary. Relish this final moments, for soon you will be nothing more than mindless undead!', '', '', '', '', '', '', '', '', 16748, '1', '0', '0', 'tyrannus_intro2');
+INSERT INTO `script_texts` VALUES ('-1610202', 'Your last waking memory will be of agonising pain!', '', '', '', '', '', '', '', '', 16749, '1', '0', '0', 'tyrannus_intro3');
+INSERT INTO `script_texts` VALUES ('-1610203', 'Tyrannus transforms the champions into undead.', '', '', '', '', '', '', '', '', 16750, '2', '0', '0', 'tyrannus_intro4');
+INSERT INTO `script_texts` VALUES ('-1610204', 'Minions! Destroy these interlopers!', '', '', '', '', '', '', '', '', 16751, '1', '0', '0', 'tyrannus_intro5');
+-- Garfrost
+INSERT INTO `script_texts` VALUES ('-1610051', 'Tiny creatures under feet, you bring Garfrost something good to eat!', null, null, null, null, null, null, null, null, 16912, '1', '0', '0', 'aggro');
+INSERT INTO `script_texts` VALUES ('-1610052', 'Will save for snack. For later.', null, null, null, null, null, null, null, null, 16913, '1', '0', '0', 'slay');
+INSERT INTO `script_texts` VALUES ('-1610053', 'Forgemaster Garfrost hurls a massive saronite boulder at you!', null, null, null, null, null, null, null, null, '0', '3', '0', '0', 'saronite_emote');
+INSERT INTO `script_texts` VALUES ('-1610054', 'That one maybe not so good to eat now. Stupid Garfrost! BAD! BAD!', null, null, null, null, null, null, null, null, 16914, '1', '0', '0', 'slay2');
+INSERT INTO `script_texts` VALUES ('-1610055', 'Axe too weak. Garfrost make better and CRUSH YOU!', null, null, null, null, null, null, null, null,16916, '1', '0', '0', '66%');
+INSERT INTO `script_texts` VALUES ('-1610056', 'Garfrost tired of puny mortals. Now your bones will freeze!', null, null, null, null, null, null, null, null, 16917, '1', '0', '0', '33%');
+INSERT INTO `script_texts` VALUES ('-1610057', 'Garfrost hope giant underpants clean. Save boss great shame. For later.', null, null, null, null, null, null, null, null, 16915, '1', '0', '0', 'death');
+INSERT INTO `script_texts` VALUES ('-1610058', 'Another shall take his place. You waste your time.', null, null, null, null, null, null, null, null, 16752, '1', '0', '0', 'tyranus_outro');
+INSERT INTO `script_texts` VALUES ('-1610059', 'The forgemaster is dead! Get geared up men, we have a Scourgelord to kill.', null, null, null, null, null, null, null, null, '0', '1', '0', '0', 'slave_outro');
+-- Ick & Krick
+INSERT INTO `script_texts` VALUES ('-1610060', 'Our work must not be interrupted! Ick! Take care of them!', null, null, null, null, null, null, null, null, 16926, '1', '0', '0', 'aggro');
+INSERT INTO `script_texts` VALUES ('-1610061', 'Quickly! Poison them all while they\'re still close!', null, null, null, null, null, null, null, null, 16930, '1', '0', '0', 'poison_nova');
+INSERT INTO `script_texts` VALUES ('-1610062', 'Enough moving around! Hold still while I blow them all up!', null, null, null, null, null, null, null, null, 16929, '1', '0', '0', 'explosive_barrage');
+INSERT INTO `script_texts` VALUES ('-1610063', 'Krick begins rapidly conjuring explosive mines!', null, null, null, null, null, null, null, null, '0', '3', '0', '0', 'ick_explosion');
+INSERT INTO `script_texts` VALUES ('-1610064', 'I\'ve changed my mind...go get that one instead!', null, null, null, null, null, null, null, null, 16931, '1', '0', '0', 'pursue1');
+INSERT INTO `script_texts` VALUES ('-1610065', 'No! That one! That one! Get that one!', null, null, null, null, null, null, null, null, 16932, '1', '0', '0', 'pursue2');
+INSERT INTO `script_texts` VALUES ('-1610066', 'What are you attacking him for? The dangerous one is over there,fool!', null, null, null, null, null, null, null, null, 16933, '1', '0', '0', 'pursue3');
+INSERT INTO `script_texts` VALUES ('-1610067', 'Ooh...We could probably use these parts!', null, null, null, null, null, null, null, null, 16927, '1', '0', '0', 'slay1');
+INSERT INTO `script_texts` VALUES ('-1610068', 'Arms and legs are in short supply...Thanks for your contribution!', null, null, null, null, null, null, null, null, 16928, '1', '0', '0', 'slay2');
+-- Krick outro:
+INSERT INTO `script_texts` VALUES ('-1610069', 'Wait! Stop! Don\'t kill me, please! I\'ll tell you everything!', null, null, null, null, null, null, null, null, 16934, '1', '0', '0', 'krick_stop');
+INSERT INTO `script_texts` VALUES ('-1610070', 'I\'m not so naive as to believe your appeal for clemency, but I will listen.', null, null, null, null, null, null, null, null, 16611, '1', '0', '0', 'outro1_aly');
+INSERT INTO `script_texts` VALUES ('-1610071', 'Why should the Banshee Queen spare your miserable life?', null, null, null, null, null, null, null, null, 17033, '1', '0', '0', 'outro1_horde');
+INSERT INTO `script_texts` VALUES ('-1610072', 'What you seek is in the master\'s lair, but you must destroy Tyrannus to gain entry. Within the Halls of Reflection you will find Frostmourne. It... it holds the truth.', null, null, null, null, null, null, null, null, 16935, '1', '0', '0', 'outro2_krick');
+INSERT INTO `script_texts` VALUES ('-1610073', 'Frostmourne lies unguarded? Impossible!', null, null, null, null, null, null, null, null, 16612, '1', '0', '0', 'outro3_aly');
+INSERT INTO `script_texts` VALUES ('-1610074', 'Frostmourne? The Lich King is never without his blade! If you are lying to me...', null, null, null, null, null, null, null, null, 17034, '1', '0', '0', 'outro3_horde');
+INSERT INTO `script_texts` VALUES ('-1610075', 'I swear it is true! Please, don\'t kill me!!', null, null, null, null, null, null, null, null, 16936, '1', '0', '0', 'outro4_krick');
+INSERT INTO `script_texts` VALUES ('-1610076', 'Worthless gnat! Death is all that awaits you!', null, null, null, null, null, null, null, null, 16753, '1', '0', '0', 'outro5_tyrannus');
+INSERT INTO `script_texts` VALUES ('-1610077', 'Urg... no!!', null, null, null, null, null, null, null, null, 16937, '1', '0', '0', 'outro6_krick');
+INSERT INTO `script_texts` VALUES ('-1610078', 'Do not think that I shall permit you entry into my master\'s sanctum so easily. Pursue me if you dare.', null, null, null, null, null, null, null, null, 16754, '1', '0', '0', 'outro7_tyrannus');
+INSERT INTO `script_texts` VALUES ('-1610079', 'What a cruel end. Come, heroes. We must see if the gnome\'s story is true. If we can separate Arthas from Frostmourne, we might have a chance at stopping him', null, null, null, null, null, null, null, null, 16613, '1', '0', '0', 'outro8_aly');
+INSERT INTO `script_texts` VALUES ('-1610080', 'A fitting end for a traitor. Come, we must free the slaves and see what is within the Lich King\'s chamber for ourselves.', null, null, null, null, null, null, null, null, 17035, '1', '0', '0', 'outro8_horde');
+INSERT INTO `script_texts` VALUES ('-1610081', 'Your pursuit shall be in vain, adventurers, for the Lich King has placed an army of undead at my command! Behold!', null, null, null, null, null, null, null, null, 16755, '1', '0', '0', 'ambush1_tyrannus');
+INSERT INTO `script_texts` VALUES ('-1610082', 'Persistent whelps! You will not reach the entrance of my lord\'s lair! Soldiers, destroy them!', null, null, null, null, null, null, null, null, 16756, '1', '0', '0', 'ambush2_tyrannus');
+-- Tyrannus
+INSERT INTO `script_texts` VALUES ('-1610083', 'Rimefang! Trap them within the tunnel! Bury them alive!', null, null, null, null, null, null, null, null, 16757, '1', '0', '0', 'gauntlet');
+INSERT INTO `script_texts` VALUES ('-1610084', 'Alas, brave, brave adventurers, your meddling has reached its end. Do you hear the clatter of bone and steel coming up the tunnel behind you? That is the sound of your impending demise.', null, null, null, null, null, null, null, null, 16758, '1', '0', '0', 'intro1');
+INSERT INTO `script_texts` VALUES ('-1610085', 'Heroes! We will hold off the undead as long as we can, even to our dying breath. Deal with the Scourgelord!', null, null, null, null, null, null, null, null, 0, '1', '0', '0', 'intro2');
+INSERT INTO `script_texts` VALUES ('-1610086', 'Ha, such an amusing gesture from the rabble. When I have finished with you, my master\'s blade will feast upon your souls. Die!', null, null, null, null, null, null, null, null, 16759, '1', '0', '0', 'intro3');
+INSERT INTO `script_texts` VALUES ('-1610087', 'I shall not fail The Lich King! Come and meet your end!', null, null, null, null, null, null, null, null, 16760, '1', '0', '0', 'aggro');
+INSERT INTO `script_texts` VALUES ('-1610088', 'Scourgelord Tyrannus roars and swells with dark might!', null, null, null, null, null, null, null, null, '0', '3', '0', '0', 'unholy_power');
+INSERT INTO `script_texts` VALUES ('-1610089', 'Power... overwhelming!', null, null, null, null, null, null, null, null, 16765, '1', '0', '0', 'unholy_power');
+INSERT INTO `script_texts` VALUES ('-1610090', 'Rimefang, destroy this fool!', null, null, null, null, null, null, null, null, 16764, '1', '0', '0', 'mark_of_rimefang');
+INSERT INTO `script_texts` VALUES ('-1610091', 'Perhaps you should have stayed in the mountains!', null, null, null, null, null, null, null, null, 16762, '1', '0', '0', 'slay2');
+INSERT INTO `script_texts` VALUES ('-1610092', 'Such a shameful display... You are better off dead!', null, null, null, null, null, null, null, null, 16761, '1', '0', '0', 'slay1');
+INSERT INTO `script_texts` VALUES ('-1610093', 'Impossible! Rimefang...Warn...-', null, null, null, null, null, null, null, null, 16763, '1', '0', '0', 'death');
+-- Outro
+INSERT INTO `script_texts` VALUES ('-1610094', 'Brave champions, we owe you our lives, our freedom... Though it be a tiny gesture in the face of this enormous debt, I pledge that from this day forth, all will know of your deeds, and the blazing path of light you cut through the shadow of this dark citadel.', null, null, null, null, null, null, null, null, '0', '1', '0', '0', 'outro1');
+INSERT INTO `script_texts` VALUES ('-1610095', 'This day will stand as a testament not only to your valor, but to the fact that no foe, not even the Lich King himself, can stand when Alliance and Horde set aside their differences and ---', null, null, null, null, null, null, null, null, '0', '1', '0', '0', 'outro2');
+INSERT INTO `script_texts` VALUES ('-1610096', 'Take cover behind me! Quickly!', null, null, null, null, null, null, null, null, 17037, '1', '0', '0', 'outro4_horde');
+INSERT INTO `script_texts` VALUES ('-1610097', 'Heroes, to me!', null, null, null, null, null, null, null, null, 16614, '1', '0', '0', 'outro4_aly');
+INSERT INTO `script_texts` VALUES ('-1610098', 'I thought he\'d never shut up. At last, Sindragosa silenced that long-winded fool. To the Halls of Reflection, champions! Our objective is near... I can sense it.', null, null, null, null, null, null, null, null, 17036, '1', '0', '0', 'outro5_horde');
+INSERT INTO `script_texts` VALUES ('-1610099', 'The Frost Queen is gone. We must keep moving - our objective is near.', null, null, null, null, null, null, null, null, 16615, '1', '0', '0', 'outro5_aly');
+INSERT INTO `script_texts` VALUES ('-1610100', 'I... I could not save them... Damn you, Arthas! DAMN YOU!', null, null, null, null, null, null, null, null, 16616, '1', '0', '0', 'outro6_aly');
