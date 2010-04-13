64624e9fa1f88dd96b61ba8d461ae4aab7b9d51b
Xfurry <mail@nomail.com>
2010-04-13 17:44:24 +0300

    Reverted SP_03

--- sql/Custom_Updates/Mangos/Northrend/AzjolNerub_Mangos.sql
+++
@@ -1,5 +0,0 @@
-/* AZJOL-NERUB */
-UPDATE `gameobject_template` SET `faction` = 114 where `entry` in (192395);
-update creature_template set AIName = '', scriptName = 'npc_watcher_gashra' where entry = 28730;
-update creature_template set AIName = '', scriptName = 'npc_watcher_silthik' where entry = 28731;
-update creature_template set AIName = '', scriptName = 'npc_watcher_narjil' where entry = 28729;
