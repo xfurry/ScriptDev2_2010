87139d0c5dd2419dfa3cdb560e94fc4fcd7841a8
Xfurry <mail@nomail.com>
2010-04-15 15:50:17 +0300

    Update for [SP_27]

--- sql/Custom_Updates/Mangos/EasternKingdoms/Karazhan_Mangos.sql
+++ sql/Custom_Updates/Mangos/EasternKingdoms/Karazhan_Mangos.sql
@@ -1,2 +1,5 @@
 /* KARAZHAN */
 UPDATE `creature_template` SET `ScriptName` = 'boss_nightbane' WHERE `entry` = 17225;
+-- Chess event loot chest
+Update `gameobject` set `spawntimesecs` = -604800 where `id` = 185119;
+update gameobject_template set faction = 0, data15 = 1 where entry = 185119;
