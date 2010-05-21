df91ae77939e957711999642d9bae9e41ac16e9a
Xfurry <mail@nomail.com>
2010-05-21 19:33:46 +0300

    Graveyard update for Toc - until some core bugs get fixed

---
+++ sql/Custom_Updates/Mangos/Graveyard_Toc_mangos.sql
@@ -0,0 +1,5 @@
+-- Update the graveyard link for Toc until some bugs are fixed
+-- original graveyard: id = 1478; toc area = 4722 = 12s time/dist
+-- id = 1397 northred icecrown glacyer = 30s time/dist
+-- id = 1396 vyrkyl central = 80s time/dist - better
+update game_graveyard_zone set id = 1396 where ghost_zone = 4722;
