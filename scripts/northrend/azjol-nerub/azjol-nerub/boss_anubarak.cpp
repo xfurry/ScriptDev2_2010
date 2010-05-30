6d5cad18421821bb6c99be8f5068ff227e5b426d
The_Game_Master <mail@nomail.com>
2010-05-30 23:02:31 +0300

    Reverted Azjol-nerub changes. Actual files not removed for reimplementation.

--- scripts/northrend/azjol-nerub/azjol-nerub/boss_anubarak.cpp
+++ scripts/northrend/azjol-nerub/azjol-nerub/boss_anubarak.cpp
@@ -36,43 +36,8 @@ enum
     SAY_LOCUST_1                    = -1601021,
     SAY_LOCUST_2                    = -1601022,
     SAY_LOCUST_3                    = -1601023,
-    SAY_DEATH                       = -1601024, 
-
-    ACHIEV_GOTTA_GO                 = 1860,
+    SAY_DEATH                       = -1601024
 };
-
-enum Spells
-{
-    SPELL_CARRION_BEETLES                  =       53520,
-    SPELL_SUMMON_CARRION_BEETLES           =       53521,
-    SPELL_LEECHING_SWARM                   =       53467,
-
-    SPELL_IMPALE                           =       53454,
-    H_SPELL_IMPALE                         =       59446,
-
-    SPELL_POUND                            =       53472,
-    H_SPELL_POUND                          =       59433,
-
-    SPELL_SUBMERGE                         =       53421,
-};
-
-enum Creatures
-{
-    CREATURE_GUARDIAN                      =       29216,
-    CREATURE_VENOMANCER                    =       29217,
-    CREATURE_DATTER                        =       29213
-};
-
-#define SPAWNPOINT_Z                               224.3f
-
-float SpawnPoint[2][2] =
-{
-    {550.7f, 282.8f},
-    {551.1f, 229.4f},
-};
-
-#define HOME_X                      551.776978f
-#define HOME_Y                      254.246994f
 
 /*######
 ## boss_anubarak
@@ -90,50 +55,13 @@ struct MANGOS_DLL_DECL boss_anubarakAI : public ScriptedAI
     ScriptedInstance* m_pInstance;
     bool m_bIsRegularMode;
 
-    bool Channeling;
-    bool Summoned_Guardian;
-    bool Summoned_Venomancer;
-    bool Summoned_Datter;
-    uint32 Phase;
-    uint32 Phase_Time;
-
-    uint32 AchievTimer;
-
-    uint32 SPELL_CARRION_BEETLES_Timer;
-    uint32 SPELL_LEECHING_SWARM_Timer;
-    uint32 SPELL_IMPALE_Timer;
-    uint32 SPELL_POUND_Timer;
-    uint32 SPELL_SUBMERGE_Timer;
-    uint32 UNDERGROUND_Timer;
-    uint32 VENOMANCER_Timer;
-    uint32 DATTER_Timer;
-
     void Reset()
     {
-        SPELL_CARRION_BEETLES_Timer = 8000;
-        SPELL_LEECHING_SWARM_Timer = 20000;
-        SPELL_IMPALE_Timer = 9000;
-        SPELL_POUND_Timer = 15000;
-
-        Phase = 0;
-        Phase_Time = 0;
-        Channeling = false;
-
-        AchievTimer = 0;
-
-        m_creature->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NON_ATTACKABLE|UNIT_FLAG_NOT_SELECTABLE);
-        m_creature->RemoveAurasDueToSpell(SPELL_SUBMERGE);
-        
-        if (m_pInstance)
-            m_pInstance->SetData(TYPE_ANUBARAK, NOT_STARTED);
     }
 
     void Aggro(Unit* pWho)
     {
         DoScriptText(SAY_AGGRO, m_creature);
-
-        if (m_pInstance)
-            m_pInstance->SetData(TYPE_ANUBARAK, IN_PROGRESS);
     }
 
     void KilledUnit(Unit* pVictim)
@@ -149,15 +77,6 @@ struct MANGOS_DLL_DECL boss_anubarakAI : public ScriptedAI
     void JustDied(Unit* pKiller)
     {
         DoScriptText(SAY_DEATH, m_creature);
-
-        if (m_pInstance)
-            m_pInstance->SetData(TYPE_ANUBARAK, DONE);
-
-        if (!m_bIsRegularMode && AchievTimer < 240000)
-        {
-            if(m_pInstance)
-                m_pInstance->DoCompleteAchievement(ACHIEV_GOTTA_GO);
-        }
     }
 
     void UpdateAI(const uint32 uiDiff)
@@ -165,143 +84,7 @@ struct MANGOS_DLL_DECL boss_anubarakAI : public ScriptedAI
         if (!m_creature->SelectHostileTarget() || !m_creature->getVictim())
             return;
 
-        //Achievement counter
-        AchievTimer += uiDiff;
-
-        if (Channeling == true)
-        {
-            for (uint8 i = 0; i < 4; ++i)
-                DoCast(m_creature->getVictim(), SPELL_SUMMON_CARRION_BEETLES, true);
-            Channeling = false;
-        }
-
-        if (Phase == 1)
-        {
-            if (SPELL_IMPALE_Timer <= uiDiff)
-            {
-                if (Unit* pTarget = m_creature->SelectAttackingTarget(ATTACKING_TARGET_RANDOM, 0))
-                {
-                if (DoCastSpellIfCan(pTarget, m_bIsRegularMode ? SPELL_IMPALE : H_SPELL_IMPALE) == CAST_OK)
-                    SPELL_IMPALE_Timer = 9000;
-                }
-            } else SPELL_IMPALE_Timer -= uiDiff;
-
-            if (!Summoned_Guardian)
-            {
-                for (uint8 i = 0; i < 2; ++i)
-                {
-                    if (Creature *Guardian = m_creature->SummonCreature(CREATURE_GUARDIAN,SpawnPoint[i][0],SpawnPoint[i][1],SPAWNPOINT_Z,0,TEMPSUMMON_CORPSE_DESPAWN,0))
-                    {
-                        Guardian->AddThreat(m_creature->getVictim(), 0.0f);
-                    }
-                }
-                Summoned_Guardian = true;
-            }
-
-            if(!Summoned_Venomancer)
-            {
-                if (VENOMANCER_Timer <= uiDiff)
-                {
-                    if (Phase_Time > 1)
-                    {
-                        for (uint8 i = 0; i < 2; ++i)
-                        {
-                            if (Creature *Venomancer = m_creature->SummonCreature(CREATURE_VENOMANCER,SpawnPoint[i][0],SpawnPoint[i][1],SPAWNPOINT_Z,0,TEMPSUMMON_CORPSE_DESPAWN,0))
-                            {
-                                Venomancer->AddThreat(m_creature->getVictim(), 0.0f);
-                            }
-                        }
-                        Summoned_Venomancer = true;
-                    }
-                } else VENOMANCER_Timer -= uiDiff;
-            }
-
-            if(!Summoned_Datter)
-            {
-                if (DATTER_Timer <= uiDiff)
-                {
-                    if (Phase_Time > 2)
-                    {
-                        for (uint8 i = 0; i < 2; ++i)
-                        {
-                            if (Creature *Datter = m_creature->SummonCreature(CREATURE_DATTER,SpawnPoint[i][0],SpawnPoint[i][1],SPAWNPOINT_Z,0,TEMPSUMMON_CORPSE_DESPAWN,0))
-                            {
-                                Datter->AddThreat(m_creature->getVictim(), 0.0f);
-                            }
-                        }
-                        Summoned_Datter = true;
-                    }
-                } else DATTER_Timer -= uiDiff;
-            }
-
-            if (UNDERGROUND_Timer <= uiDiff)
-            {
-                m_creature->RemoveAurasDueToSpell(SPELL_SUBMERGE);
-                m_creature->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NON_ATTACKABLE|UNIT_FLAG_NOT_SELECTABLE);
-                Phase = 0;
-            } else UNDERGROUND_Timer -= uiDiff;
-        }
-
-        if (Phase == 0)
-        {
-            if (SPELL_LEECHING_SWARM_Timer <= uiDiff)
-            {
-			switch(urand(0, 2))
-                {
-                case 0: DoScriptText(SAY_LOCUST_1, m_creature); break;
-                case 1: DoScriptText(SAY_LOCUST_2, m_creature); break;
-                case 2: DoScriptText(SAY_LOCUST_3, m_creature); break;
-                }
-                DoCast(m_creature, SPELL_LEECHING_SWARM, true);
-                SPELL_LEECHING_SWARM_Timer = 19000;
-            } else SPELL_LEECHING_SWARM_Timer -= uiDiff;
-
-            if (SPELL_CARRION_BEETLES_Timer <= uiDiff)
-            {
-                Channeling = true;
-                DoCastSpellIfCan(m_creature, m_bIsRegularMode ? SPELL_CARRION_BEETLES : SPELL_CARRION_BEETLES);
-                SPELL_CARRION_BEETLES_Timer = 25000;
-            } else SPELL_CARRION_BEETLES_Timer -= uiDiff;
-
-            if (SPELL_POUND_Timer <= uiDiff)
-            {
-                 if (DoCastSpellIfCan(m_creature, m_bIsRegularMode ? SPELL_POUND : H_SPELL_POUND) == CAST_OK)
-                    SPELL_POUND_Timer = 16500;
-            } else SPELL_POUND_Timer -= uiDiff;
-        }
-
-        if ((Phase_Time == 0 && m_creature->GetHealth()*100 / m_creature->GetMaxHealth() <= 75)
-            || (Phase_Time == 1 && m_creature->GetHealth()*100 / m_creature->GetMaxHealth() <= 50)
-            || (Phase_Time == 2 && m_creature->GetHealth()*100 / m_creature->GetMaxHealth() <= 25))
-        {
-            ++Phase_Time;
-
-            Summoned_Guardian = false;
-            Summoned_Venomancer = false;
-            Summoned_Datter = false;
-
-            UNDERGROUND_Timer = 40000;
-            VENOMANCER_Timer = 25000;
-            DATTER_Timer = 32000;
-
-            DoCast(m_creature, SPELL_SUBMERGE, false);
-
-            m_creature->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NON_ATTACKABLE|UNIT_FLAG_NOT_SELECTABLE);
-
-            Phase = 1;
-
-            switch(urand(0, 1))
-                {
-                case 0: DoScriptText(SAY_SUBMERGE_1, m_creature); break;
-                case 1: DoScriptText(SAY_SUBMERGE_2, m_creature); break;
-                }
-        }
-
-        if (Phase != 1)
-            DoMeleeAttackIfReady();
-
-        if (m_creature->GetDistance2d(HOME_X, HOME_Y) > 30)
-            EnterEvadeMode();
+        DoMeleeAttackIfReady();
     }
 };
 
--- scripts/northrend/azjol-nerub/azjol-nerub/boss_krikthir.cpp
+++ scripts/northrend/azjol-nerub/azjol-nerub/boss_krikthir.cpp
@@ -42,56 +42,6 @@ enum
     EMOTE_BOSS_GENERIC_FRENZY       = -1000005
 };
 
-enum Spells
-{
-    SPELL_MIND_FLAY                        =   52586,
-    H_SPELL_MIND_FLAY                      =   59367,
-    SPELL_CURSE_OF_FATIGUE                 =   52592,
-    H_SPELL_CURSE_OF_FATIGUE               =   59368,
-    SPELL_FRENZY                           =   28747, //maybe 53361
-
-    SPELL_SUMMON_SKITTERING_SWARMER        =   52438, //AOE Effect 140, maybe 52439
-    SPELL_SUMMON_SKITTERING_SWARMER_1      =   52439, //Summon 3x 28735
-
-    H_SPELL_ACID_SPLASH                    =   59363,
-    SPELL_ACID_SPLASH                      =   52446,
-
-    SPELL_CHARGE                           =   16979,//maybe is another spell
-    SPELL_BACKSTAB                         =   52540,
-
-    SPELL_SHADOW_BOLT                      =   52534,
-    H_SPELL_SHADOW_BOLT                    =   59357,
-    SPELL_SHADOW_NOVA                      =   52535,
-    H_SPELL_SHADOW_NOVA                    =   59358,
-
-    SPELL_STRIKE                           =   52532,
-    SPELL_CLEAVE                           =   49806,
-
-    SPELL_ENRAGE                           =   52470,
-
-    SPELL_INFECTED_BITE                    =   52469,
-    H_SPELL_INFECTED_BITE                  =   59364,
-    SPELL_WEB_WRAP                         =   52086,//the spell is not working propperly
-
-    SPELL_BLINDING_WEBS                    =   52524,
-    H_SPELL_BLINDING_WEBS                  =   59365,
-
-    SPELL_POSION_SPRAY                     =   52493,
-    H_SPELL_POSION_SPRAY                   =   59366,
-};
-
-enum Mobs
-{
-    MOB_SKITTERING_SWARMER                 =   28735,
-    MOB_SKITTERING_SWARMER_CONTROLLER      =   32593,
-    MOB_SKITTERING_INFECTIOR               =   28736,
-};
-
-enum Misc
-{
-    ACHIEVEMENT_WATCH_HIM_DIE              =   1296
-};
-
 /*######
 ## boss_krikthir
 ######*/
@@ -108,46 +58,13 @@ struct MANGOS_DLL_DECL boss_krikthirAI : public ScriptedAI
     ScriptedInstance* m_pInstance;
     bool m_bIsRegularMode;
 
-    uint32 MindFlayTimer;
-    uint32 CurseFatigueTimer;
-    uint32 SummonTimer;
-    uint32 m_uiSpeakTimer;
-
     void Reset()
     {
-        MindFlayTimer       = 15000;
-        CurseFatigueTimer   = 12000;
-        m_uiSpeakTimer      = 15000;
-
-        if (m_pInstance)
-            m_pInstance->SetData(TYPE_KRIKTHIR, NOT_STARTED);
     }
 
     void Aggro(Unit* pWho)
     {
         DoScriptText(SAY_AGGRO, m_creature);
-
-        Summon();
-        SummonTimer = 15000;
-
-        if (m_pInstance)
-            m_pInstance->SetData(TYPE_KRIKTHIR, IN_PROGRESS);
-
-        Creature* pGashra = ((Creature*)Unit::GetUnit((*m_creature), m_pInstance->GetData64(DATA_WATCHER_GASHRA)));
-        Creature* pSilthik = ((Creature*)Unit::GetUnit((*m_creature), m_pInstance->GetData64(DATA_WATCHER_SILTHIK)));
-        Creature* pNarjil = ((Creature*)Unit::GetUnit((*m_creature), m_pInstance->GetData64(DATA_WATCHER_NARJIL)));;
-
-        if (pGashra && pSilthik && pNarjil)
-        {
-            if (m_creature->getVictim())
-            {
-                pGashra->AI()->AttackStart(m_creature->getVictim());
-                pSilthik->AI()->AttackStart(m_creature->getVictim());
-                pNarjil->AI()->AttackStart(m_creature->getVictim());
-            }
-        }
-
-        m_creature->CallForHelp(50.0f);
     }
 
     void KilledUnit(Unit* pVictim)
@@ -163,45 +80,6 @@ struct MANGOS_DLL_DECL boss_krikthirAI : public ScriptedAI
     void JustDied(Unit* pKiller)
     {
         DoScriptText(SAY_DEATH, m_creature);
-
-        if (m_pInstance)
-            m_pInstance->SetData(TYPE_KRIKTHIR, DONE);
-
-        Creature* pGashra = ((Creature*)Unit::GetUnit((*m_creature), m_pInstance->GetData64(DATA_WATCHER_GASHRA)));
-        Creature* pSilthik = ((Creature*)Unit::GetUnit((*m_creature), m_pInstance->GetData64(DATA_WATCHER_SILTHIK)));
-        Creature* pNarjil = ((Creature*)Unit::GetUnit((*m_creature), m_pInstance->GetData64(DATA_WATCHER_NARJIL)));;
-
-        if (pGashra && pSilthik && pNarjil)
-            if(pGashra->isAlive() && pSilthik->isAlive() && pNarjil->isAlive() && !m_bIsRegularMode)
-            {
-                if(m_pInstance)
-                    m_pInstance->DoCompleteAchievement(ACHIEVEMENT_WATCH_HIM_DIE);
-            }
-    }
-
-    void Summon()
-    {
-            m_creature->SummonCreature(MOB_SKITTERING_SWARMER,566.164f,682.087f,769.079f,2.21657f,TEMPSUMMON_TIMED_DESPAWN,25000);
-            m_creature->SummonCreature(MOB_SKITTERING_SWARMER,566.164f,682.087f,769.079f,2.21657f,TEMPSUMMON_TIMED_DESPAWN,25000);
-            m_creature->SummonCreature(MOB_SKITTERING_SWARMER,529.042f,706.941f,777.298f,1.0821f,TEMPSUMMON_TIMED_DESPAWN,25000);
-            m_creature->SummonCreature(MOB_SKITTERING_SWARMER,529.042f,706.941f,777.298f,1.0821f,TEMPSUMMON_TIMED_DESPAWN,25000);
-            m_creature->SummonCreature(MOB_SKITTERING_SWARMER,489.975f,671.239f,772.131f,0.261799f,TEMPSUMMON_TIMED_DESPAWN,25000);
-            m_creature->SummonCreature(MOB_SKITTERING_SWARMER,489.975f,671.239f,772.131f,0.261799f,TEMPSUMMON_TIMED_DESPAWN,25000);
-            m_creature->SummonCreature(MOB_SKITTERING_SWARMER,488.556f,692.95f,771.764f,4.88692f,TEMPSUMMON_TIMED_DESPAWN,25000);
-            m_creature->SummonCreature(MOB_SKITTERING_SWARMER,488.556f,692.95f,771.764f,4.88692f,TEMPSUMMON_TIMED_DESPAWN,25000);
-            m_creature->SummonCreature(MOB_SKITTERING_INFECTIOR,553.34f,640.387f,777.419f,1.20428f,TEMPSUMMON_TIMED_DESPAWN,25000);
-            m_creature->SummonCreature(MOB_SKITTERING_SWARMER,553.34f,640.387f,777.419f,1.20428f,TEMPSUMMON_TIMED_DESPAWN,25000);
-            m_creature->SummonCreature(MOB_SKITTERING_INFECTIOR,517.486f,706.398f,777.335f,5.35816f,TEMPSUMMON_TIMED_DESPAWN,25000);
-            m_creature->SummonCreature(MOB_SKITTERING_SWARMER,517.486f,706.398f,777.335f,5.35816f,TEMPSUMMON_TIMED_DESPAWN,25000);
-            m_creature->SummonCreature(MOB_SKITTERING_INFECTIOR,504.01f,637.693f,777.479f,0.506145f,TEMPSUMMON_TIMED_DESPAWN,25000);
-            m_creature->SummonCreature(MOB_SKITTERING_SWARMER,504.01f,637.693f,777.479f,0.506145f,TEMPSUMMON_TIMED_DESPAWN,25000);
-            m_creature->SummonCreature(MOB_SKITTERING_SWARMER,552.625f,706.408f,777.177f,3.4383f,TEMPSUMMON_TIMED_DESPAWN,25000);
-            m_creature->SummonCreature(MOB_SKITTERING_SWARMER,552.625f,706.408f,777.177f,3.4383f,TEMPSUMMON_TIMED_DESPAWN,25000);
-    }
-
-    void JustSummoned(Creature* summoned)
-    {
-        summoned->GetMotionMaster()->MovePoint(m_creature->GetEntry(),m_creature->GetPositionX(),m_creature->GetPositionY(),m_creature->GetPositionZ());
     }
 
     void UpdateAI(const uint32 uiDiff)
@@ -209,236 +87,13 @@ struct MANGOS_DLL_DECL boss_krikthirAI : public ScriptedAI
         if (!m_creature->SelectHostileTarget() || !m_creature->getVictim())
             return;
 
-        //Sounds Krikthir
-        if(!m_creature->getVictim())
-        {
-            if(m_uiSpeakTimer < uiDiff)
-            {
-		        switch (rand()%3)
-		        {
-			        case 0: DoPlaySoundToSet(m_creature, SAY_PREFIGHT_1); break;
-			        case 1: DoPlaySoundToSet(m_creature, SAY_PREFIGHT_2); break;
-			        case 2: DoPlaySoundToSet(m_creature, SAY_PREFIGHT_3); break;
-		        }
-                m_uiSpeakTimer = 20000;
-            }else m_uiSpeakTimer -= uiDiff;
-        }
-
-        if (SummonTimer <= uiDiff)
-        {
-            switch(urand(0, 1))
-            {
-                case 0: DoScriptText(SAY_SWARM_1, m_creature); break;
-                case 1: DoScriptText(SAY_SWARM_2, m_creature); break;
-            }
-            Summon();
-            SummonTimer = 15000;
-        } else SummonTimer -= uiDiff;
-
-
-        if (MindFlayTimer <= uiDiff)
-        {
-                if (DoCastSpellIfCan(m_creature, m_bIsRegularMode ? SPELL_MIND_FLAY : H_SPELL_MIND_FLAY) == CAST_OK)
-                MindFlayTimer = 20000;
-            } else MindFlayTimer -= uiDiff;
-
-        if (CurseFatigueTimer <= uiDiff)
-        {
-            //WowWiki say "Curse of Fatigue-Kirk'thir will cast Curse of Fatigue on 2-3 targets periodically."
-            if (Unit* pTarget = m_creature->SelectAttackingTarget(ATTACKING_TARGET_RANDOM, 0))
-            {
-                if (DoCastSpellIfCan(pTarget, m_bIsRegularMode ? SPELL_CURSE_OF_FATIGUE : H_SPELL_CURSE_OF_FATIGUE) == CAST_OK)
-                    CurseFatigueTimer = 10000;
-            }
-
-               if (Unit* pTarget_1 = m_creature->SelectAttackingTarget(ATTACKING_TARGET_RANDOM, 1))
-            {
-                if (DoCastSpellIfCan(pTarget_1, m_bIsRegularMode ? SPELL_CURSE_OF_FATIGUE : H_SPELL_CURSE_OF_FATIGUE) == CAST_OK)
-                    CurseFatigueTimer = 10000;
-            }
-
-            CurseFatigueTimer = 10000;
-        } else CurseFatigueTimer -= uiDiff;
-
-        if (!m_creature->HasAura(SPELL_FRENZY) && m_creature->GetHealth()*100 / m_creature->GetMaxHealth() <= 10)
-        {
-            DoScriptText(EMOTE_BOSS_GENERIC_FRENZY, m_creature);
-            DoCast(m_creature, SPELL_FRENZY, true);
-        }
-
         DoMeleeAttackIfReady();
-    }
-};
-
-struct MANGOS_DLL_DECL npc_watcher_gashraAI : public ScriptedAI
-{
-    npc_watcher_gashraAI(Creature *pCreature) : ScriptedAI(pCreature)
-    {
-        m_pInstance = (ScriptedInstance*)pCreature->GetInstanceData();
-        m_bIsRegularMode = pCreature->GetMap()->IsRegularDifficulty();
-    }
-    bool m_bIsRegularMode;
-    ScriptedInstance* m_pInstance;
-
-    uint32 WebWrapTimer;
-    uint32 InfectedBiteTimer;
-
-    void Reset()
-    {
-        WebWrapTimer      = 11000;
-        InfectedBiteTimer = 4000;
-    }
-
-    void EnterCombat(Unit* who)
-    {
-        DoCast(m_creature, SPELL_ENRAGE, true);
-    }
-
-    void UpdateAI(const uint32 diff)
-    {
-        if (!m_creature->SelectHostileTarget() || !m_creature->getVictim())
-            return;
-
-        if (WebWrapTimer <= diff)
-        {
-            if (Unit* pTarget = m_creature->SelectAttackingTarget(ATTACKING_TARGET_RANDOM, 0))
-            {
-                DoCast(pTarget, SPELL_WEB_WRAP, true);
-                WebWrapTimer = 17000;
-            }
-        } else WebWrapTimer -= diff;
-
-        if (InfectedBiteTimer <= diff)
-        {
-            DoCast(m_creature, m_bIsRegularMode ? SPELL_INFECTED_BITE : H_SPELL_INFECTED_BITE);
-            InfectedBiteTimer = 15000;
-        } else InfectedBiteTimer -= diff;
-
-        DoMeleeAttackIfReady();
-    }
-};
-
-struct MANGOS_DLL_DECL npc_watcher_narjilAI : public ScriptedAI
-{
-    npc_watcher_narjilAI(Creature *pCreature) : ScriptedAI(pCreature)
-    {
-        m_pInstance = (ScriptedInstance*)pCreature->GetInstanceData();
-        m_bIsRegularMode = pCreature->GetMap()->IsRegularDifficulty();
-    }
-    bool m_bIsRegularMode;
-    ScriptedInstance* m_pInstance;
-
-    uint32 WebWrapTimer;
-    uint32 InfectedBiteTimer;
-    uint32 BlindingWebsTimer;
-
-    void Reset()
-    {
-        WebWrapTimer      = 11000;
-        InfectedBiteTimer = 4000;
-        BlindingWebsTimer = 17000;
-    }
-
-    void UpdateAI(const uint32 diff)
-    {
-        if (!m_creature->SelectHostileTarget() || !m_creature->getVictim())
-            return;
-
-        if (WebWrapTimer <= diff)
-        {
-            if (Unit* pTarget = m_creature->SelectAttackingTarget(ATTACKING_TARGET_RANDOM, 0))
-            {
-                DoCast(pTarget, SPELL_WEB_WRAP, true);
-                WebWrapTimer = 15000;
-            }
-        } else WebWrapTimer -= diff;
-
-        if (InfectedBiteTimer <= diff)
-        {
-            DoCast(m_creature, m_bIsRegularMode ? SPELL_INFECTED_BITE : H_SPELL_INFECTED_BITE);
-            InfectedBiteTimer = 11000;
-        } else InfectedBiteTimer -= diff;
-
-        if (BlindingWebsTimer <= diff)
-        {
-            DoCast(m_creature, m_bIsRegularMode ? SPELL_BLINDING_WEBS : H_SPELL_BLINDING_WEBS);
-            BlindingWebsTimer = 17000;
-        } else BlindingWebsTimer -= diff;
-
-        DoMeleeAttackIfReady();
-    }
-};
-
-struct MANGOS_DLL_DECL npc_watcher_silthikAI : public ScriptedAI
-{
-    npc_watcher_silthikAI(Creature *pCreature) : ScriptedAI(pCreature)
-    {
-        m_pInstance = (ScriptedInstance*)pCreature->GetInstanceData();
-        m_bIsRegularMode = pCreature->GetMap()->IsRegularDifficulty();
-    }
-    bool m_bIsRegularMode;
-    ScriptedInstance* m_pInstance;
-
-    uint32 WebWrapTimer;
-    uint32 InfectedBiteTimer;
-    uint32 PosionSprayTimer;
-
-    void Reset()
-    {
-        WebWrapTimer      = 11000;
-        InfectedBiteTimer = 4000;
-        PosionSprayTimer  = 15000;
-    }
-
-    void UpdateAI(const uint32 diff)
-    {
-        if (!m_creature->SelectHostileTarget() || !m_creature->getVictim())
-            return;
-
-        if (WebWrapTimer <= diff)
-        {
-            if (Unit* pTarget = m_creature->SelectAttackingTarget(ATTACKING_TARGET_RANDOM, 0))
-            {
-                DoCast(pTarget, SPELL_WEB_WRAP, true);
-                WebWrapTimer = 15000;
-            }
-        } else WebWrapTimer -= diff;
-
-        if (InfectedBiteTimer <= diff)
-        {
-            DoCast(m_creature, m_bIsRegularMode ? SPELL_INFECTED_BITE : H_SPELL_INFECTED_BITE);
-                    InfectedBiteTimer = 15000;
-        } else InfectedBiteTimer -= diff;
-
-        if (PosionSprayTimer <= diff)
-        {
-            DoCast(m_creature, m_bIsRegularMode ? SPELL_POSION_SPRAY : H_SPELL_POSION_SPRAY);
-                    PosionSprayTimer = 17000;
-        } else PosionSprayTimer -= diff;
-
-        DoMeleeAttackIfReady();
-
     }
 };
 
 CreatureAI* GetAI_boss_krikthir(Creature* pCreature)
 {
     return new boss_krikthirAI(pCreature);
-}
-
-CreatureAI* GetAI_npc_watcher_gashra (Creature* pCreature)
-{
-    return new npc_watcher_gashraAI (pCreature);
-}
-
-CreatureAI* GetAI_npc_watcher_narjil (Creature* pCreature)
-{
-    return new npc_watcher_narjilAI (pCreature);
-}
-
-CreatureAI* GetAI_npc_watcher_silthik (Creature* pCreature)
-{
-    return new npc_watcher_silthikAI (pCreature);
 }
 
 void AddSC_boss_krikthir()
@@ -448,20 +103,5 @@ void AddSC_boss_krikthir()
     newscript = new Script;
     newscript->Name = "boss_krikthir";
     newscript->GetAI = &GetAI_boss_krikthir;
-    newscript->RegisterSelf();
-
-    newscript = new Script;
-    newscript->Name = "npc_watcher_gashra";
-    newscript->GetAI = &GetAI_npc_watcher_gashra;
-    newscript->RegisterSelf();
-
-    newscript = new Script;
-    newscript->Name = "npc_watcher_silthik";
-    newscript->GetAI = &GetAI_npc_watcher_silthik;
-    newscript->RegisterSelf();
-
-    newscript = new Script;
-    newscript->Name = "npc_watcher_narjil";
-    newscript->GetAI = &GetAI_npc_watcher_narjil;
     newscript->RegisterSelf();
 }
