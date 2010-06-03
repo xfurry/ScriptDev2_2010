39a17c29771d65d2a0b838d5b1e8d0a4deba9116
Pesci <danielturcu03@yahoo.com>
2010-06-03 15:47:49 +0300

    Revert "Training dummy doesn't follow the attacker any more."
    This reverts commit ac80a2caff89d52262d3d5d1779d5b83006ed04d.

--- scripts/world/npcs_special.cpp
+++ scripts/world/npcs_special.cpp
@@ -1753,48 +1753,6 @@ bool GossipSelect_npc_experience_eliminator(Player* pPlayer, Creature* pCreature
     pPlayer->CLOSE_GOSSIP_MENU();
     return true;
 }
-/*######
-## npc_training_dummy
-######*/
-
-#define OUT_OF_COMBAT_TIME 5000
-
-struct MANGOS_DLL_DECL npc_training_dummyAI : public Scripted_NoMovementAI
-{
-    uint32 combat_timer;
-
-    npc_training_dummyAI(Creature* pCreature) : Scripted_NoMovementAI(pCreature)
-    {
-        Reset();
-    }
-
-    void Reset()
-    {
-        combat_timer = 0;
-    }
-
-    void DamageTaken(Unit* pDoneBy, uint32 &uiDamage)
-    {
-        combat_timer = 0;
-    }
-
-    void UpdateAI(const uint32 diff)
-    {
-        if (!m_creature->SelectHostileTarget() || !m_creature->getVictim())
-            return;
-
-        m_creature->ModifyHealth(m_creature->GetMaxHealth());
-
-        combat_timer += diff;
-        if (combat_timer > OUT_OF_COMBAT_TIME)
-            EnterEvadeMode();
-    }
-};
-
-CreatureAI* GetAI_npc_training_dummy(Creature* pCreature)
-{
-    return new npc_training_dummyAI(pCreature);
-}
 
 void AddSC_npcs_special()
 {
@@ -1890,10 +1848,5 @@ void AddSC_npcs_special()
     newscript->Name = "npc_experience_eliminator";
     newscript->pGossipHello = &GossipHello_npc_experience_eliminator;
     newscript->pGossipSelect = &GossipSelect_npc_experience_eliminator;
-    newscript->RegisterSelf();
-	
-	newscript = new Script;
-    newscript->Name = "npc_training_dummy";
-    newscript->GetAI = &GetAI_npc_training_dummy;
     newscript->RegisterSelf();
 }
