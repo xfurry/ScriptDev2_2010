05375ebd11d6f519d7a940206c078dda05cf696b
Xfurry <radu_cantor@yahoo.com>
2010-08-11 21:08:59 +0300

    Update radius for ToC

--- scripts/northrend/crusaders_coliseum/trial_of_the_crusader/boss_northrend_beasts.cpp
+++ scripts/northrend/crusaders_coliseum/trial_of_the_crusader/boss_northrend_beasts.cpp
@@ -1782,7 +1782,8 @@ struct MANGOS_DLL_DECL mob_slime_poolAI : public ScriptedAI
     void Reset()
     {
         DoCast(m_creature, SPELL_SLIME_POOL_VISUAL);
-        m_fSize = 1.0f;
+		m_fSize = 0.5f;
+		m_creature->SetFloatValue(OBJECT_FIELD_SCALE_X, m_fSize);
         m_uiSizeTimer   = 1000;
         m_uiSpellTimer  = 1000;
         m_bHasSpell     = false;
@@ -1790,7 +1791,7 @@ struct MANGOS_DLL_DECL mob_slime_poolAI : public ScriptedAI
 
     void UpdateAI(const uint32 uiDiff)
     {
-        if(m_uiSizeTimer < uiDiff && m_fSize <= 2.0f)
+        if(m_uiSizeTimer < uiDiff && m_fSize <= 1.25f)
         {
 			if(Difficulty == RAID_DIFFICULTY_10MAN_NORMAL || Difficulty == RAID_DIFFICULTY_25MAN_NORMAL)
 				m_fSize += 0.033f;
