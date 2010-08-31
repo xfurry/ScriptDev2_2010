736236fa7f90e19426075cbe68dc3bb6da033294
Xfurry <radu_cantor@yahoo.com>
2010-09-01 00:09:02 +0300

    Update for TK - Alar

--- sql/Custom_Updates/Mangos/Outland/TheEye_Mangos.sql
+++ sql/Custom_Updates/Mangos/Outland/TheEye_Mangos.sql
@@ -1,5 +1,5 @@
 /* TK: THE EYE */
--- Al'ar
+-- Al'ar 
 update `creature_template` set `ScriptName`='boss_alar' where `entry`='19514';
 update `creature_template` set `ScriptName`='mob_ember_of_alar' where `entry`='19551';
 update `creature_template` set `ScriptName`='mob_flame_patch_alar' where `entry`='20602';
