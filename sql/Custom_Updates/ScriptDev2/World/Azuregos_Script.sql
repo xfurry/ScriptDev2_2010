8c22b6cc23b978c5500c24d50f3d3150220c6f82
The_Game_Master <mail@nomail.com>
2010-10-13 01:00:54 +0300

    Typo from prev commit.

--- sql/Custom_Updates/ScriptDev2/World/Azuregos_Script.sql
+++ sql/Custom_Updates/ScriptDev2/World/Azuregos_Script.sql
@@ -1,4 +1,4 @@
-##TextIDs
+/* Azuregos */
 DELETE FROM `script_texts` WHERE `entry` = '-1630010';
 INSERT INTO script_texts (entry, emote, content_default, comment) VALUES
 (-1630010, 0, "I said GOOD DAY!", "SAY_SpiritAzu_GoodBye");
